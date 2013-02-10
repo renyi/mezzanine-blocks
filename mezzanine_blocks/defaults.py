52113b6b0a6fa5b84fe207aeddfba16f05722a0c
Renyi Khor <renyi.ace@gmail.com>
2013-02-11 03:43:13 +0800

    Moved cache prefix settings to defaults.py.

---
+++ mezzanine_blocks/defaults.py
@@ -0,0 +1,10 @@
+from django.utils.translation import ugettext as _
+from mezzanine.conf import register_setting
+
+
+register_setting(
+    name="MEZZANINE_BLOCKS_CACHE_PREFIX",
+    description=_("Block cache prefix."),
+    editable=False,
+    default="mezzanine_blocks.",
+)
